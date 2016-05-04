b8d9b12d58c1ee182b52d50aa1fb081cb1e6a79d
Bruno Costa <bruno.costa@hardquarters.com>
2016-05-04 16:59:33 +0100

    Revert "[fix] timezone issue in production"
    This reverts commit 43a32aecb66b49701951099b359a61500d39fb49.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.time_zone = 'Europe/Lisbon'
-  config.active_record.default_timezone = :local
 end
